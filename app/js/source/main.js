db9404adaac62576590a69094d85dfb7cabf19e9
Carter Nelms <carternelms@bellsouth.net>
2014-04-11 15:57:21 -0500

    friday's end

---
+++ app/js/source/main.js
@@ -0,0 +1,89 @@
+(function()
+{
+	'use strict';
+
+	$(document).ready(initialize);
+
+	function initialize()
+	{
+		$('#title').click(title);
+		$('.number').click(number);
+		$('.clear').click(clear);
+		$('#decimal').click(decimal);
+		$('#sign').click(sign);
+	}
+
+	function title()
+	{
+		var disp = $('#calculator').css('display');
+
+		if(disp === 'none')
+		{
+			$('#calculator').fadeIn();
+		}
+		else
+		{
+			$('#calculator').fadeOut();
+		}
+	}
+
+	function getDisplay()
+	{
+		return $('#display').text();
+	}
+
+	function setDisplay(disp)
+	{
+		$('#display').text(disp);
+	}
+
+	function number()
+	{
+		var num = this.textContent;
+		var disp = getDisplay();
+		disp = (disp !== '0') ? disp + num : num;
+
+		setDisplay(disp);
+	}
+
+	function clear()
+	{
+		if(this.textContent === 'C')
+		{
+			setDisplay('0');
+		}
+	}
+
+	function decimal()
+	{
+		var disp = getDisplay();
+		disp += (disp.indexOf('.') === -1) ? '.' : '';
+		setDisplay(disp);
+	}
+
+	function sign()
+	{
+		var disp = getDisplay();
+		// if(disp * 1 !=== 0)
+		// {
+		// 	disp
+		// }
+
+
+		if(disp.indexOf('-') === -1)
+		{
+			if(disp * 1 !== 0)
+			{
+				disp = '-' + disp;
+			}
+		}
+		else
+		{
+			disp = disp.split('-')[1];
+			// Remove - sign
+		}
+
+		setDisplay(disp);
+	}
+
+})();
